df690f6b0f5990c0f91c48ca6c0fd6085dc9176b
Mustapha Najib <91792932+najib153@users.noreply.github.com>
2025-04-12 19:02:23 -0400

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,4 +1,6 @@
-import pandas as pd
+import os
 
 def load_data(filepath: str) -> pd.DataFrame:
+    if not os.path.exists(filepath):
+        raise FileNotFoundError(f" File not found: {filepath}. Please upload it to the app's root directory.")
     return pd.read_csv(filepath)
