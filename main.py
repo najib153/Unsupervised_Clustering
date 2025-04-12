702c365e551823830e5443c18674aaa8b39c6a4c
Mustapha Najib <91792932+najib153@users.noreply.github.com>
2025-04-12 18:33:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from src.data_loader import load_data
-from src.preprocessing import train_kmeans, evaluate_kmeans
-from src.visualization import plot_clusters, plot_elbow, plot_silhouette
+from data_loader import load_data
+from preprocessing import train_kmeans, evaluate_kmeans
+from visualization import plot_clusters, plot_elbow, plot_silhouette
 
 df = load_data("data/Mall_Customers.csv")
 features = ["Annual_Income", "Spending_Score"]
