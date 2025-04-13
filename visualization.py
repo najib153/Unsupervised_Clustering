e2e0c42fa98a2cfe0fe47e7860d12225786756c2
Mustapha Najib <91792932+najib153@users.noreply.github.com>
2025-04-12 20:06:03 -0400

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -1,21 +1,26 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
+import streamlit as st
 
 def plot_clusters(df: pd.DataFrame, x: str, y: str, hue: str = "Cluster"):
-    sns.scatterplot(x=x, y=y, data=df, hue=hue, palette="colorblind")
-    plt.show()
+    fig, ax = plt.subplots()
+    sns.scatterplot(x=x, y=y, data=df, hue=hue, palette="colorblind", ax=ax)
+    ax.set_title("K-Means Clusters")
+    st.pyplot(fig)
 
 def plot_elbow(df: pd.DataFrame):
-    df.plot(x="cluster", y="WSS_Score")
-    plt.xlabel("No. of clusters")
-    plt.ylabel("WSS Score")
-    plt.title("Elbow Plot")
-    plt.show()
+    fig, ax = plt.subplots()
+    df.plot(x="cluster", y="WSS_Score", marker='o', ax=ax)
+    ax.set_xlabel("No. of clusters")
+    ax.set_ylabel("WSS Score")
+    ax.set_title("Elbow Plot")
+    st.pyplot(fig)
 
 def plot_silhouette(df: pd.DataFrame):
-    df.plot(x="cluster", y="Silhouette_Score")
-    plt.xlabel("No. of clusters")
-    plt.ylabel("Silhouette Score")
-    plt.title("Silhouette Plot")
-    plt.show()
+    fig, ax = plt.subplots()
+    df.plot(x="cluster", y="Silhouette_Score", marker='o', color='green', ax=ax)
+    ax.set_xlabel("No. of clusters")
+    ax.set_ylabel("Silhouette Score")
+    ax.set_title("Silhouette Plot")
+    st.pyplot(fig)
